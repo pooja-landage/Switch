8c1cc4ff7211a5204e731e911ed48c3796dfc0be
pooja-landage <poojalandage22@gmail.com>
2021-09-16 07:58:39 +0530

    Add project files.

---
+++ Pratice8Switch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Pratice8Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("===================Switch================");
+
+            int day;
+            Console.WriteLine("Enter any day in number");
+            day = Convert.ToInt32(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("The day is Monday");
+                    break;
+
+                case 2:
+                    Console.WriteLine("The day is Tuesday");
+                    break;
+
+                default:
+                    Console.WriteLine("Incorrect day");
+                    break;
+
+
+
+
+            }
+
+        }
+    }
+}
